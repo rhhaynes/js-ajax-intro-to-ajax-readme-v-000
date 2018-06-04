fc2c10012987b9d3f60a9e48f415f19a912420df
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-04 15:05:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener("load", showCommits);
-  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
-  req.send()
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
 }
